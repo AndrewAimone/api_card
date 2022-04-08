4e0abdd07a43b584fb0e789c9211699d2535bd9e
AndrewAimone <andrewaimone.info@gmail.com>
2022-04-08 00:48:23 -0300

    Start Project

---
+++ src/main/java/days/apicard/ApicardApplication.java
@@ -0,0 +1,13 @@
+package days.apicard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicardApplication.class, args);
+	}
+
+}
